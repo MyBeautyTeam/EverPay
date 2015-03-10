d887bdaf4f4b53007d922d5368ed3c847cdcc59a
Pashna <p02p@yandex.ru>
2015-03-11 01:34:03 +0400

    +Drawer Start

--- EverPay/app/src/main/java/com/beautyteam/everpay/MainActivity123.java
+++ EverPay/app/src/main/java/com/beautyteam/everpay/MainActivity123.java
@@ -1,29 +1,46 @@
 package com.beautyteam.everpay;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.beautyteam.everpay.Adapters.DebtorsListAdapter;
 import com.beautyteam.everpay.Adapters.PageAdapter;
 import com.beautyteam.everpay.Fragments.MainPageFragment;
 
-import it.neokree.materialtabs.MaterialTab;
-import it.neokree.materialtabs.MaterialTabHost;
-import it.neokree.materialtabs.MaterialTabListener;
+//import it.neokree.materialtabs.MaterialTab;
+//import it.neokree.materialtabs.MaterialTabHost;
+//import it.neokree.materialtabs.MaterialTabListener;
 
 /**
  * Created by Admin on 07.03.2015.
  */
-public class MainActivity123 extends ActionBarActivity implements MaterialTabListener {
+public class MainActivity123 extends ActionBarActivity {//} implements MaterialTabListener {
 
     ViewPager viewPager;
     PageAdapter pageAdapter;
-    MaterialTabHost tabHost;
+//    MaterialTabHost tabHost;
+
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private CharSequence mTitle;
+    private ActionBarDrawerToggle mDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +48,67 @@ public class MainActivity123 extends ActionBarActivity implements MaterialTabLis
         setContentView(R.layout.main_acrivity123);
 
         viewPager = (ViewPager) findViewById(R.id.pager);
-        tabHost = (MaterialTabHost) this.findViewById(R.id.materialTabHost);
+//        tabHost = (MaterialTabHost) this.findViewById(R.id.materialTabHost);
 
         pageAdapter = new PageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pageAdapter);
 
-        for (int i = 0; i < pageAdapter.getCount(); i++) {
+  /*      for (int i = 0; i < pageAdapter.getCount(); i++) {
             tabHost.addTab(
                     tabHost.newTab()
                             .setText(pageAdapter.getPageTitle(i))
                             .setTabListener(this)
             );
-        }
+        }*/
 
         //PagerTabStrip tabStrip = (PagerTabStrip) findViewById(R.id.pagerTabStrip);
 
         //FragmentTransaction fTran = getSupportFragmentManager().beginTransaction();
         //fTran.replace(R.id.mainLayout, MainPageFragment.getInstance());
         //fTran.commit();
+
+        //============= DRAWER
+        mTitle = "EverPay";
+
+        mPlanetTitles = new String[]{"Главная", "Группы", "Выход"};
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_item, mPlanetTitles));
+
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, /* host Activity */
+                mDrawerLayout, /* DrawerLayout object */
+                R.drawable.ic_menu_white_18dp, /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open, /* "open drawer" description */
+                R.string.drawer_close /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle("ФАК");
+                invalidateOptionsMenu();
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        //=============
     }
 
-    @Override
+/*    @Override
     public void onTabSelected(MaterialTab tab) {
         viewPager.setCurrentItem(tab.getPosition());
     }
@@ -61,5 +118,63 @@ public class MainActivity123 extends ActionBarActivity implements MaterialTabLis
     @Override
     public void onTabUnselected(MaterialTab tab) {
     }
+*/
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+// Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+// Pass the event to ActionBarDrawerToggle, if it returns
+// true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+// Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Swaps fragments in the main content view
+     */
+    private void selectItem(int position) {
+        Toast.makeText(this, R.string.app_name, Toast.LENGTH_SHORT).show();
+
+// Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
 }
