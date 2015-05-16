1ad4ff42f00c47a84b9ff77cc66cdb3f744a4738
Tan <tanyadmp@mail.ru>
2015-05-16 13:07:07 +0300

    add dialog send

--- EverPay/app/src/main/java/com/beautyteam/everpay/Fragments/FragmentGroupDetails.java
+++ EverPay/app/src/main/java/com/beautyteam/everpay/Fragments/FragmentGroupDetails.java
@@ -190,6 +190,8 @@ public class FragmentGroupDetails extends Fragment implements View.OnClickListen
             History.TEXT_WHO_SAY,
             History.TEXT_WHAT_WHOM,
             History.TEXT_DESCRIPTION,
+            History.STATE,
+            History.RESULT
 
     };
 
