49bf9ac9f352231b23633611601550ff6e04bad5
Pashna <p02p@yandex.ru>
2015-08-15 18:24:18 +0400

    Для Татьяны

--- EverPay/app/src/main/java/com/beautyteam/everpay/Fragments/FragmentAddBill.java
+++ EverPay/app/src/main/java/com/beautyteam/everpay/Fragments/FragmentAddBill.java
@@ -487,7 +487,7 @@ public class FragmentAddBill extends Fragment implements
      */
     private int insertToDB() {
 
-        Cursor maxCursor = getActivity().getContentResolver().query(EverContentProvider.BILLS_CONTENT_URI, new String [] {"MAX("+Bills.BILL_ID+")"}, null, null, null);
+        Cursor maxCursor = getActivity().getContentResolver().query(EverContentProvider.BILLS_CONTENT_URI, new String[]{"MAX(" + Bills.BILL_ID + ")"}, null, null, null);
         maxCursor.moveToFirst();
         int max = maxCursor.getInt(0);
         int billID = max+1;
@@ -532,8 +532,11 @@ public class FragmentAddBill extends Fragment implements
                     else {
                         indexOfShowcase++;
                         show.hide();
+                        LinearLayout child = (LinearLayout) getViewByPosition(1);
+                        View put = child.findViewById(R.id.add_bill_list_put_parrent);
+
                         show =  new ShowcaseView.Builder(getActivity())
-                                .setTarget(new ViewTarget(R.id.add_bill_name_put, getActivity()))
+                                .setTarget(new ViewTarget(put))
                                 .setContentTitle("В ячейку под полем ВНЕС впишите сумму, которую внес каждый участник")
                                 .setStyle(R.style.CustomShowcaseTheme2)
                                 .setScaleMultiplier(0.5f)
@@ -713,5 +716,17 @@ private class SwitchChangeListener implements CompoundButton.OnCheckedChangeList
         show.hide();
         super.onStop();
 
+    }
+
+    private View getViewByPosition(int pos) {
+        final int firstListItemPosition = addBillList.getFirstVisiblePosition();
+        final int lastListItemPosition = firstListItemPosition + addBillList.getChildCount() - 1;
+
+        if (pos < firstListItemPosition || pos > lastListItemPosition ) {
+            return addBillList.getAdapter().getView(pos, null, addBillList);
+        } else {
+            final int childIndex = pos - firstListItemPosition;
+            return addBillList.getChildAt(childIndex);
+        }
     }
 }
